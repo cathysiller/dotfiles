4bdc8d963b43bd98b2b5c89e5d927a9645c6b789
Catherine Siller <csiller@mailgun.com>
2021-08-23 13:41:36 -0500

    added bash profile

---
+++ bash_profile.js
@@ -0,0 +1,20 @@
+parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+PS1="\[\e[1;91m\][\w]\$(parse_git_branch)\[\e[0m\] "
+
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nv$
+
+
+
+if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
+  . `brew --prefix`/etc/bash_completion.d/git-completion.bash
+fi
+
+
+
+
